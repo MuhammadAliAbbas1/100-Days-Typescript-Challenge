c86809915ac283f3d4985f98a220ad9c232ab0a9
Muhammad Ali Abbas <165416981+MuhammadAliAbbas1@users.noreply.github.com>
2024-04-20 03:22:33 +0500

    Delete Day 2/Q.3/Q.4 directory

--- Day 2/Q.3/Q.4/question4.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let name4 = 'Bertand Russel';
-let quote = `${name4} once said,"To understand the actual word as it is,not as we should wish it to be,is the beginning of wisdom."`;
-console.log(quote);
--- Day 2/Q.3/Q.4/question4.ts
+++
@@ -1,3 +0,0 @@
-let name4: string = 'Bertand Russel';
-let quote: string = `${name4} once said,"To understand the actual word as it is,not as we should wish it to be,is the beginning of wisdom."`;
-console.log(quote);
