ea377f13b87063f77de81dee9fa2db0d0c67ded2
Muhammad Ali Abbas <165416981+MuhammadAliAbbas1@users.noreply.github.com>
2024-04-20 03:28:24 +0500

    Add files via upload

---
+++ Day 2/Q.5/question5.js
@@ -0,0 +1,4 @@
+"use strict";
+let name5 = "\t\n Abbas \t\n";
+console.log(name5);
+console.log(name5.trim());
---
+++ Day 2/Q.5/question5.ts
@@ -0,0 +1,4 @@
+let name5: string = "\t\n Abbas \t\n";
+console.log(name5);
+console.log(name5.trim());
+
