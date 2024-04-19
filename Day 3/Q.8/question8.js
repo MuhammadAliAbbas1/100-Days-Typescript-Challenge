018c10787fbbf023273cbdf9b0d891458569d9a2
Muhammad Ali Abbas <165416981+MuhammadAliAbbas1@users.noreply.github.com>
2024-04-20 03:53:49 +0500

    Add files via upload

---
+++ Day 3/Q.8/question8.js
@@ -0,0 +1,4 @@
+"use strict";
+// Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+let favouriteNumber = 4;
+console.log(`My favourite number is ${favouriteNumber}`);
---
+++ Day 3/Q.8/question8.ts
@@ -0,0 +1,4 @@
+// Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+
+let favouriteNumber: number= 4
+console.log(`My favourite number is ${favouriteNumber}`);
