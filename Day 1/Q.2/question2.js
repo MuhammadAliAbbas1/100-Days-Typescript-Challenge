886a3f241fe29946f98cdb153d341a0f8f686ab7
Muhammad Ali ABbas <aliabbasdodi@gmail.com>
2024-04-20 02:44:16 +0500

    first commit

---
+++ Day 1/Q.2/question2.js
@@ -0,0 +1,5 @@
+"use strict";
+let name2 = "Hayyan King";
+console.log(name2.toLowerCase());
+console.log(name2.toUpperCase());
+console.log(name2.charAt(0).toUpperCase() + name2.slice(1).toLowerCase());
